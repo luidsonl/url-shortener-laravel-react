9fd3d58b35653a343cc5c08521001ddd1c78b425
Luidson Lima <luidsonlsti@gmail.com>
2025-11-10 18:12:04 -0300

    refact: update subscription event type enum

--- backend/app/Enums/SubscriptionEventType.php
+++ backend/app/Enums/SubscriptionEventType.php
@@ -7,7 +7,5 @@ enum SubscriptionEventType: string
     case CREATED = 'created';
     case RENEWED = 'renewed';
     case EXPIRED = 'expired';
-    case PLAN_CHANGED = 'plan_changed';
-    case CREDIT_APPLIED = 'credit_applied';
     case CANCELLED = 'cancelled';
 }
