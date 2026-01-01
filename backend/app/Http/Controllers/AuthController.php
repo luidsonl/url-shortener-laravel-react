bad612e4cf8e9cc40697168bad76fa5a55d4e4f2
Luidson Lima <luidsonlsti@gmail.com>
2026-01-01 20:22:40 -0300

    feat: change min password for register to 8

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         $user = User::create([
